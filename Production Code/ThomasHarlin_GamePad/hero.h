999558d584cb78d1a38baa3dc8822e4d02c79e3c
Thomas Harlin <ThomasHarlin@users.noreply.github.com>
2021-04-07 23:30:01 -0400

    Character is displayed and moves

--- Production Code/ThomasHarlin_GamePad/hero.h
+++ Production Code/ThomasHarlin_GamePad/hero.h
@@ -12,10 +12,13 @@ int heroH = BeachcomberBarry_H + margin;
 float heroSpeed = 1.0;
 
 //tracks the direction hero is facing
-//true = left, false = right;
-bool heroDir = true;
+//false = left, true = right
+bool heroDir = false;
+//false = standing animation, true = walking animation
+int heroMove = false;
 
-int heroFrame;
+int heroFrame = 2;
+int heroOffset = 4;
 Metro heroFrameTimer = Metro(250);
 
 
@@ -25,13 +28,58 @@ void drawHero() {
   heroX = heroX + (joyBuffer[0] * heroSpeed);
   heroY = heroY + (joyBuffer[1] * heroSpeed);
 
+
+
+  //sees if the player will be doing a move animation
+  if (joyBuffer[0] == 0 && joyBuffer[1] == 0) {
+    heroMove = false;
+  }
+  else {
+    heroMove = true;
+  }
+  //tracks which way the character is moving
+  if (joyBuffer[0] == -1) {
+    heroDir = false;
+  }
+  if (joyBuffer[0] == 1) {
+    heroDir = true;
+  }
+
+
+
+  if (heroFrameTimer.check()) {
+    heroFrame++;
+
+    //is the joystick being touched?
+    if (heroMove) {
+      //moving right animation
+      if (heroDir) {
+        if (heroFrame > 5 || heroFrame < 2) {
+          heroFrame = 2;
+        }
+      }
+      //moving left animation
+      else {
+        if (heroFrame > 9 || heroFrame < 6) {
+          heroFrame = 6;
+        }
+      }
+    }
+    //Standng Still Animation
+    else {
+      if (heroFrame > 1) {
+        heroFrame = 0;
+      }
+    }
+  }
+
+
+  //draws the background level and character
   drawLevel(1);
-
   tft.setClipRect(heroX - margin, heroY - margin, heroW, heroH);
-
-  tft.drawRGBBitmap(heroX, heroY, BeachcomberBarry_PIX[0], BeachcomberBarry_MASK[0], BeachcomberBarry_W, BeachcomberBarry_H);
-
+  tft.drawRGBBitmap(heroX, heroY, BeachcomberBarry_PIX[heroFrame], BeachcomberBarry_MASK[heroFrame], BeachcomberBarry_W, BeachcomberBarry_H);
   tft.updateScreen();
+
 
 }
 //////////////////////////////////////////////////////////////////////////////////////
